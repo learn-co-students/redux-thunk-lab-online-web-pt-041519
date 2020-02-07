a73ce02cc2cfd8915c21eae1d72187526c3a8778
Meg Galvez <meg@oakmaine.com>
2020-02-07 11:06:14 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
 
 class App extends Component {   
+
+  componentDidMount() {
+    this.props.fetchCats()
+  }
+
+  // renderCatPics = () =>{
+  //   )
+  // }
   
   render() {
     return (
-      <div>
+      <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        <ul>
+        {this.props.catPics.map((pic) => <CatList catPics={pic.url} key={pic.id} />)}
+        </ul>
       </div>
     );
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,16 @@
-// write your CatList component here
+import React, { Component } from 'react'
+
+class CatList extends Component {
+
+
+    render(){
+        console.log(this.props)
+        return (
+            <li key={this.props.key}>
+                <img src={this.props.catPics} alt=''></img>
+            </li>
+        )
+    }
+}
+
+export default CatList
