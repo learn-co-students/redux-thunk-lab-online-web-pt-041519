765e16b0ed342a38bdf57a7b55f1df6e30f3ede3
github username <github email address>
2020-02-16 09:48:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+// 1. set up provider and needed middleware in index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import catsReducer from './reducers/catsReducer'
 
+// inport applyMiddle from redux
+import { applyMiddleware } from 'redux'
 
+// import thunk from redux-thunk
+import thunk from 'redux-thunk'
+
+// create the store - pass the reducer into it (we haven't written it yet)
+// we pass applymiddleware in as second argument to createStore
+const store = createStore(catsReducer, applyMiddleware(thunk))
+
+// wrap the app in the provider - pass store as props to provider
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+    <App />
+    </Provider>,
     document.getElementById('root')
 );
--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,29 @@
 // add the catsReducer
+
+// pass in original state as an argument to catsReducer, 
+// it is a blank array with default loading status of false - meaning we haven't fetched yet
+
+const catsReducer = (state = { cats: [], loading: false}, action) => {
+   // set up a switch that handles two actions: 'LOADING_CATS' and 'ADD_CATS'
+   switch(action.type) {
+      case 'LOADING_CATS': 
+         return {
+            ...state,
+            cats: [...state.cats],
+            loading: true
+         }
+      case 'ADD_CATS':
+         return {
+            // return a shallow copy of the state because we can't mutate the actual state
+            ...state,
+            // set the cats key of the state copy to whatever we're bringing back after the fetch call
+            cats: action.cats,
+            // set loading to false because we have successfully fetched
+            loading: false
+         }
+      default:
+         return state
+   }
+}
+
+export default catsReducer
