828e1c73476beb4b9b657e4b7e84b72c65fc0a93
Michael Chen <michaelchengaming@gmail.com>
2020-02-17 23:05:44 -0800

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,21 @@
 // write and export your action creator function here
+
+export const fetchCats = () => {
+  try {
+    return dispatch => {
+      // dispatch({ type: "LOADING_CATS" })
+      // const res = await fetch(
+      //   "https://learn-co-curriculum.github.io/cat-api/cats.json"
+      //   );
+      //   const data = await res.json();
+      //   await dispatch({ type: "ADD_CATS", payload: data.images });
+      //   console.log(data.images)
+      dispatch({ type: "LOADING_CATS" });
+      fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+        .then(res => res.json())
+        .then(data => dispatch({ type: "ADD_CATS", cats: data.images }));
+    };
+  } catch (error) {
+    console.log(error.message);
+  }
+};
