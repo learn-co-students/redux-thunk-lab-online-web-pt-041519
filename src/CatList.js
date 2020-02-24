5a9eaf184e0fd1a23bb2678fb857756922b43c14
github username <github email address>
2020-02-24 09:52:13 -0800

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,19 @@
 // write your CatList component here
+import React from 'react'
+
+const CatList = (props) => {
+    console.log(props)
+    // display cat pic with image url
+    const images = props.catPics.map(catPic => 
+        <div key={catPic.id}> 
+            <img src={catPic.url} alt={catPic.id} />
+        </div>)
+
+    return (
+        <div>
+            {images}
+        </div>
+    )
+}
+
+export default CatList;
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,13 @@
 // write and export your action creator function here
+function fetchCats() {
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_CATS' })
+        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+           .then(res => res.json())
+           .then(responseJSON => {
+               dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+            })
+    }
+}
+
+export default fetchCats;
